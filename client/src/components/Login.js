dee3f351ed07c60b351ce4b0c85e11cee8c534b1
Andrea Rosentel <andrearosentel@gmail.com>
2022-09-17 15:38:45 -0700

    login is working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,35 @@
+import { useState } from "react"
 import { NavButton } from "./Tools"
 
 export const Login = () => {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onLogin = e => {
+        e.preventDefault()
+        fetch('/login',  {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                username,
+                password
+            })
+        }).then(r=>{
+            if (r.ok) {
+                r.json().then(data=>console.log(data))
+            } else {
+                r.json().then(error=>console.log(error))
+            }
+        })
+    }
+
+
     return (
         <div>
-            <form>
-                <input type='text' name='username' placeholder="username"/><br/>
-                <input type='password' name='password' placeholder="password"/><br/>
+            <form onSubmit={onLogin}>
+                <input type='text' name='username' placeholder="username" onChange={e=>setUsername(e.target.value)}/><br/>
+                <input type='password' name='password' placeholder="password" onChange={e=>setPassword(e.target.value)}/><br/>
                 <button type="submit">Submit</button>
                 <NavButton />
             </form>
